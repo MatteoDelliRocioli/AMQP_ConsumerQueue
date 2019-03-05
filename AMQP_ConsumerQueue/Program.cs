44f871f5a0623046c589866679507a66afaad427
delli <matteo.delli.rocioli.pe@gmail.com>
2019-03-05 12:04:50 +0100

    Add project files.

---
+++ AMQP_ConsumerQueue/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace AMQP_ConsumerQueue
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(   queue: "queueTest", //queue name
+                                            durable: false,     //queue persistence during broker restarts
+                                            exclusive: false,   //if true makes a queue usable only by its declaring connection
+                                            autoDelete: false,  //if true deletes the queu when nobody is subsribed
+                                            arguments: null);   //plugins and queue features (ex. message-TTL, queue max-length)
+
+                    var consumer = new EventingBasicConsumer(channel); //creates a new cosnumer instance
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body); // from original bytes sent by the sender to a human-readable string
+                        Console.WriteLine(" [x] received {0", message);
+                    };
+
+                    channel.BasicConsume(queue: "queueTest",        //declares where the consumer has to consume
+                                            autoAck: true,          //sneds automatic-akcnowledgment
+                                            consumer: consumer);
+
+                    Console.WriteLine("press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
